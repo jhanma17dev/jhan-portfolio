78289086496ef4c90512265238d492839c65b2c9
jhanma17 <196592339+jhanma17dev@users.noreply.github.com>
2025-03-16 21:47:46 -0500

    Feat: Add tailwind and Daisy UI

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <NavBar></NavBar>
+  <HeroSection></HeroSection>
+  <Projects></Projects>
+  <HeroSection></HeroSection>
+</template>
+
+<script setup>
+import NavBar from './components/NavBar.vue';
+import HeroSection from './components/HeroSection.vue';
+import Projects from './components/ProjectsSection.vue';
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap');
+</style>
